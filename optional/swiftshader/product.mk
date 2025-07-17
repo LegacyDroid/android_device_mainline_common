05dbb268633eaf46ec2b46756ed813c7c457ec64
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-17 22:09:44 +0800

    fixup! mainline/common: swiftshader: Pack into APEX
    Change-Id: I6c662263848428a96c4d4072c01d88963f81aa37

--- optional/swiftshader/product.mk
+++ optional/swiftshader/product.mk
@@ -6,7 +6,7 @@
 ifeq ($(TARGET_GRAPHICS_VULKAN),swiftshader)
 
 PRODUCT_PACKAGES += \
-    org.lineageos.device.mainline.common.graphics.vulkan.swiftshader
+    org.lineageos.device.graphics.vulkan.swiftshader
 
 # TODO(b/65201432): Swiftshader needs to create executable memory.
 PRODUCT_REQUIRES_INSECURE_EXECMEM_FOR_SWIFTSHADER := true

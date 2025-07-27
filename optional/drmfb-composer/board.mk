9b99c1eb93631fa7c2af0402f350401e3bcfa4c0
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-27 17:09:49 +0800

    mainline/common: optional: Add drmfb-composer configuration
    Change-Id: Ibaf1be44dc8259d1b61b2895c7c51b377f78b303

---
+++ optional/drmfb-composer/board.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_GRAPHICS_COMPOSER_HAL),drmfb-composer)
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/mainline/interfaces/graphics/composer/drmfb/sepolicy
+
+endif # TARGET_GRAPHICS_COMPOSER_HAL

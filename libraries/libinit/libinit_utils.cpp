b8c725a5fd0511c90470fa7f11c1f17ad6e42c66
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-10-30 01:53:16 +0100

    mainline/common: libinit: Fix `const` and `static` usages
    Change-Id: I39738685bca884a5c9303d877b699d0beab9eacc

--- libraries/libinit/libinit_utils.cpp
+++ libraries/libinit/libinit_utils.cpp
@@ -19,7 +19,7 @@ void property_override(std::string prop, std::string value, bool add) {
     }
 }
 
-std::vector<std::string> ro_props_default_source_order = {
+static const std::vector<std::string> ro_props_default_source_order = {
         "odm.", "odm_dlkm.", "product.", "system.", "system_ext.", "vendor.", "vendor_dlkm.", "",
 };
 
